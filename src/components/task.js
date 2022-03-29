e9cf1650423f34dd86b6259c5227ea77b6fac86b
PrinceNiyonshuti <npprince47@gmail.com>
2022-03-29 21:19:19 +0200

    ft(Task): creating task and retrieving all task on DOM

--- src/components/task.js
+++ src/components/task.js
@@ -7,10 +7,14 @@ function task({ taskData }) {
     return (
         <div>
             {taskData.map((task) => (
-				<div className="holder" key={task.id}>
-                    <div>
+				<div className="task-holder" key={task.id}>
+                    <div className="task-content">
                         <h2>{task.name}</h2>
                         <p>{task.description}</p>
+                    </div>
+                    <div className="task-actions">
+                        <button className="btn complete-btn" type="submit">Complete</button>
+                        <button className="btn delete-btn" type="submit">Delete</button>
                     </div>
                 </div>
 			))}
