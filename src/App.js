1ffc7cd300cc605c1766b646f2f7f812c4975ffc
PrinceNiyonshuti <npprince47@gmail.com>
2022-03-30 22:19:21 +0200

    added delete functionality

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 /** @format */
 
-import {  useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import Task from "./components/task";
 import AddForm from "./components/AddForm";
 import "./App.css";
@@ -8,6 +8,27 @@ import "./index.css";
 
 function App() {
 	const [taskData, setTaskData] = useState(null);
+
+	// Remove Task
+	const deleteTask = (TaskId) => {
+		// const newTasks = taskData.filter((task) => task.id !== TaskId);
+		// setTaskData(newTasks);
+
+		const deleteTask = fetch(`http://localhost:8000/taskList/` + TaskId, {
+			method: "DELETE",
+		}).then(() => {
+			fetch(`http://localhost:8000/taskList`)
+				.then((res) => {
+					return res.json();
+				})
+				.then((data) => {
+					setTaskData(data);
+				});
+		});
+
+		// setTaskData(...taskData, deleteTask);
+	};
+
 	// Retrieve all Tasks
 	useEffect(() => {
 		fetch(`http://localhost:8000/taskList`)
@@ -23,7 +44,9 @@ function App() {
 			<h1>My Todos</h1>
 			<AddForm setTaskData={setTaskData} />
 			<br />
-			<div>{taskData && <Task taskData={taskData} />}</div>
+			<div>
+				{taskData && <Task taskData={taskData} deleteTask={deleteTask} />}
+			</div>
 		</div>
 	);
 }
